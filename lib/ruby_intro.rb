c015bb6709ac90f8ea0337be88a921258ca37265
EC2 Default User <ec2-user@ip-172-31-22-129.us-east-2.compute.internal>
2019-02-11 11:37:23 +0000

    Late commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # inject will take the values of the array and using :+ expression
+  # will connect them together
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  
+  return 0 if arr.empty?
+  arr.sort().last(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  
+  return false if arr.empty?
+  hash = Hash.new(0)
+  arr.each do |val|
+    if hash.key? val
+      return true
+    else
+      hash[n-val] = val
+    end # end else statement
+   end # end if statement  
+   
+   return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
+  
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+    # Checks string value and if it contains any consonant values
+  !!(s[0] =~ /[bcdfghjklmnpqrstvwexyz]+/i)
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+    return s.to_i(2) % 4 == 0
+  end # end if
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize isbn, price
+    
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end # End def
+  
+  def price_as_string
+    
+    format("$%.2f", @price)
+  end # end def 
 end
+
